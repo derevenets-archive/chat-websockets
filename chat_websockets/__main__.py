07987f2fc527fbc0a43c38c4ea3bc4e38d33a5f3
DerevenetsArtyom <artem@comvoy.be>
2018-12-04 21:29:45 +0200

    Add middleware for aiohttp_session

--- chat_websockets/__main__.py
+++ chat_websockets/__main__.py
@@ -1,13 +1,20 @@
+import base64
+
 from aiohttp import web
+from aiohttp_session import setup, cookie_storage
+from cryptography import fernet
 
 from .app import init_app
 
 
 def create_app() -> web.Application:
-    import aiohttp_debugtoolbar
-
     app = init_app()
-    aiohttp_debugtoolbar.setup(app, check_host=False)
+    # import aiohttp_debugtoolbar
+    # aiohttp_debugtoolbar.setup(app, check_host=False)
+
+    fernet_key = fernet.Fernet.generate_key()
+    secret_key = base64.urlsafe_b64decode(fernet_key)
+    setup(app, cookie_storage.EncryptedCookieStorage(secret_key))
 
     return app
 
