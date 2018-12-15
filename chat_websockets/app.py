1e73250c60242f2ab8140591b9209c2170f6fed3
DerevenetsArtyom <artem@comvoy.be>
2018-12-15 22:16:07 +0200

    Add middlewares list to 'init_app'

--- chat_websockets/app.py
+++ chat_websockets/app.py
@@ -1,13 +1,16 @@
+import base64
 from pathlib import Path
 from typing import Optional, List
 
 import aiohttp_jinja2
-from aiohttp import web
 import jinja2
+from aiohttp import web
+from aiohttp_session import cookie_storage, session_middleware
+from cryptography import fernet
 
 from chat_websockets.routes import init_routes
 from chat_websockets.utils.common import init_config
-
+from .middlewares import authorize
 
 path = Path(__file__).parent
 
@@ -21,7 +24,17 @@ def init_jinja2(app: web.Application) -> None:
 
 
 def init_app(config: Optional[List[str]] = None) -> web.Application:
-    app = web.Application()
+    fernet_key = fernet.Fernet.generate_key()
+    secret_key = base64.urlsafe_b64decode(fernet_key)
+
+    app = web.Application(
+        middlewares=[
+            session_middleware(
+                cookie_storage.EncryptedCookieStorage(secret_key)
+            ),
+            authorize,
+        ]
+    )
 
     init_jinja2(app)
     init_config(app, config=config)
