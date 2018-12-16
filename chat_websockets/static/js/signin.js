a5c0bb84e569b20ac9f4012cb4a51fcd1abb4922
DerevenetsArtyom <artem@comvoy.be>
2018-12-16 13:13:44 +0200

    Create signin.js to handle registration logic

---
+++ chat_websockets/static/js/signin.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+    $('#login').click(function () {
+        window.location.href = "login"
+    });
+
+    function showError(error) {
+        $('#error').html(error)
+    }
+
+    $('#submit').click(function () {
+        const login = $('#login_text').val();
+        const email = $('#email').val();
+        const password = $('#password');
+        const password2 = $('#password2');
+
+        console.log(login, email, password.val(), password2.val());
+        if (password.val() === password2.val()) {
+            console.log(login, 'if pass the same');
+            if (login && email && password) {
+                $.post('signin', {
+                    'login': login,
+                    'email': email,
+                    'password': password.val()
+                }, function (data) {
+                    console.log(data);
+                    if (data.error) {
+                        showError(data.error)
+                    } else {
+                        window.location.href = '/'
+                    }
+                });
+            } else {
+                showError('Please fill all fields')
+            }
+        } else {
+            showError('Passwords must be the same');
+        }
+    });
+});
