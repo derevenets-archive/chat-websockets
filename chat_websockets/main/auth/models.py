27fc425ae81447c1140af17223f61fdf87605fbb
DerevenetsArtyom <artem@comvoy.be>
2018-12-16 12:39:28 +0200

    Create 'User' model for 'auth' app

---
+++ chat_websockets/main/auth/models.py
@@ -0,0 +1,31 @@
+from bson.objectid import ObjectId
+
+USER_COLLECTION = 'users'
+
+
+class User:
+    def __init__(self, db, data) -> None:
+        self.db = db
+        self.collection = self.db[USER_COLLECTION]
+        self.email = data.get('email')
+        self.login = data.get('login')
+        self.password = data.get('password')
+        self.id = data.get('id')
+
+    async def check_user(self):
+        return await self.collection.find_one({'login': self.login})
+
+    async def get_login(self):
+        user = await self.collection.find_one({'_id': ObjectId(self.id)})
+        return user.get('login')
+
+    async def create_user(self):
+        user = await self.check_user()
+        result = 'User exists'
+        if not user:
+            result = await self.collection.insert({
+                'email': self.email,
+                'login': self.login,
+                'password': self.password
+            })
+        return result
