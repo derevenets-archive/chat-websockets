98d70c9c5973fd7cb7de0d4492c8a5f20a816f5a
DerevenetsArtyom <artem@comvoy.be>
2018-12-17 22:09:00 +0200

    Add 'get' method to ChatList view

--- chat_websockets/main/chat/views.py
+++ chat_websockets/main/chat/views.py
@@ -1,5 +1,13 @@
+import aiohttp_jinja2
 from aiohttp import web
+
+from .models import Message
 
 
 class ChatList(web.View):
-    pass
+
+    @aiohttp_jinja2.template('chat/index.html')
+    async def get(self):
+        message = Message(self.request.db)
+        messages = await message.get_messages()
+        return {'messages': messages}
